72f33f2083fbe8b5c8c644457ab22e540484b5e6
=aminejelass <=aminejelassi95@gmail.com>
2022-03-17 12:25:31 +0100

    python lab completed

---
+++ environment/Untitled1.py
@@ -0,0 +1,5 @@
+print("Python has three numeric types: int, float, and complex")
+myValue1=1 
+print(myValue1)
+print(type(myValue1))
+print(str(myValue1) + " is of the data type " + str(type(myValue1)))
